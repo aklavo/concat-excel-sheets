cfc8ba2cfa60b543e644c78691638e304f46cd1c
Andrew Klavekoske <aklavo@comcast.net>
2023-05-05 15:25:55 -0600

    add sys arg

--- trend-app.py
+++ trend-app.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import pandas as pd
 import dash
+import sys
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc, Input, Output
 import plotly.graph_objects as go
 
 app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
 
+full_path = str(sys.argv[1])
+split_path = full_path.rsplit('\\', 1)
+print(split_path)
 # Pull data store as df
-folder_path = r'C:\Users\andrewkl\McKinstry\MTN Projects - 12 Trends'
-file = 'Scott-BIO-BAS-Combined.csv'
-full_path = f'{folder_path}\\{file}'
+folder_path = split_path[0]
+file_name = split_path[1]
 
 df = pd.read_csv(full_path, header=0, index_col=False, parse_dates=[0])
 
@@ -19,7 +22,7 @@ fig = go.Figure()
 for column in df.columns:
     if column != 'Time':
         fig.add_trace(go.Scatter(x=df['Time'], y=df[column], name=column))
-plot_title = file.split('.')[0] + ' Plot'
+plot_title = file_name.split('.')[0] + ' Plot'
 fig.update_layout(title=plot_title,
                 xaxis_title="",
                 yaxis_title="",
