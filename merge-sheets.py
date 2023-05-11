7c40961503a05dfc0a9607ef78e978343d78227d
Andrew Klavekoske <aklavo@comcast.net>
2023-05-11 14:08:36 -0600

    handle memory issue, duplicates and !=5min points

--- merge-sheets.py
+++ merge-sheets.py
@@ -1,36 +1,25 @@
-import os
 import pandas as pd
-import sys
+import glob
 
 # Get path to data folder from user
 folder_path = str(input("Enter data folder path:"))
+output_file_name = str(input("Enter output file name:"))
 
-# Get a list of all the .csv files in the folder
-csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+# List all CSV files in the current directory
+csv_files = glob.glob(f"{folder_path}\*.csv")
 
-# Read in each .csv file store it as a dataframe, then store it in a list
+# Read each CSV file into a Pandas DataFrame and append to a list
+# Remove any duplicate datetime rows and any instances of non 5 min interval datetimes
 dfs = []
-for i, file in enumerate(csv_files):
-    df = pd.read_csv(os.path.join(folder_path, file), header=0, index_col=False, usecols=[0, 1], parse_dates=[0])
-    start_time = df['Time'].iloc[0]
-    end_time = df['Time'].iloc[-1]
-    dfs.append({'df': df, 'start_time': start_time, 'end_time': end_time})
+for file in csv_files:
+    df = pd.read_csv(file, header=0, usecols=[0, 1], parse_dates=[0])
+    df = df.drop_duplicates('Time', ignore_index=True)
+    df = df.set_index('Time')
+    df = df.asfreq('5min').dropna()
+    dfs.append(df)
 
-# Determine the maximum start and end times
-start_times = [d['start_time'] for d in dfs]
-end_times = [d['end_time'] for d in dfs]
-max_start_time = max(start_times)
-max_end_time = max(end_times)
+# Concatenate all DataFrames into a single DataFrame
+df_merged = pd.concat(dfs, axis=1, join='outer').reset_index()
 
-# Create a DataFrame with a datetime range using max/min times
-dt_range = pd.date_range(start=max_start_time, end=max_end_time, freq='5T')
-
-# Convert the datetime range to a DataFrame
-merged_df = pd.DataFrame({'Time': dt_range})
-
-# Merge all the dataframes into a single dataframe based on the Time column, NaN unavalible data
-for d in dfs:
-    merged_df = merged_df.merge(d["df"], how='left', on='Time')
-
-# Export the merged dataframe as a single .csv file
-merged_df.to_csv(os.path.join(folder_path, 'Scott-BIO-BAS-Combined.csv'), index=False)
+# Save the merged DataFrame to a CSV file
+df_merged.to_csv(f"{folder_path}\{output_file_name}", index=False)
