59713d19f5abd9adb3ee44a2937c13dec40ef3c3
Andrew Klavekoske <aklavo@comcast.net>
2023-05-11 14:30:57 -0600

    clean of chained methods

--- merge-sheets.py
+++ merge-sheets.py
@@ -5,7 +5,7 @@ import glob
 folder_path = str(input("Enter data folder path:"))
 output_file_name = str(input("Enter output file name:"))
 
-# List all CSV files in the current directory
+# List all CSV files in the directory
 csv_files = glob.glob(f"{folder_path}\*.csv")
 
 # Read each CSV file into a Pandas DataFrame and append to a list
@@ -13,13 +13,16 @@ csv_files = glob.glob(f"{folder_path}\*.csv")
 dfs = []
 for file in csv_files:
     df = pd.read_csv(file, header=0, usecols=[0, 1], parse_dates=[0])
-    df = df.drop_duplicates('Time', ignore_index=True)
-    df = df.set_index('Time')
-    df = df.asfreq('5min').dropna()
+    df = (
+        df.drop_duplicates("Time", ignore_index=True)
+        .set_index("Time")
+        .asfreq("5min")
+        .dropna()
+    )
     dfs.append(df)
 
 # Concatenate all DataFrames into a single DataFrame
-df_merged = pd.concat(dfs, axis=1, join='outer').reset_index()
+df_merged = pd.concat(dfs, axis=1, join="outer").reset_index()
 
 # Save the merged DataFrame to a CSV file
 df_merged.to_csv(f"{folder_path}\{output_file_name}", index=False)
